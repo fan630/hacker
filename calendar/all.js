18c84773778425b2dfc1be58ecbaa2ad7cc6d603
fan630 <norriswu266@gmail.com>
2020-08-05 21:34:46 +0800

    add hover

--- calendar/all.js
+++ calendar/all.js
@@ -1,15 +1,15 @@
 let date = new Date();
 let currentYear = date.getFullYear()
 let currentMonth = date.getMonth();
-console.log(currentMonth)
-
 let firstDay = new Date(currentYear, currentMonth, 1).getDay() // 星期六
 let monthDay = new Date(currentYear, currentMonth + 1, 0).getDate() // 31
-
 let dayCount = 1; // 日期變數
 let tbody = document.querySelector('tbody')
 
 function init() {
+    tbody.innerHTML = '';
+    dayCount = 1;
+    document.querySelector('.month').innerHTML = `${currentYear} ${currentMonth + 1}月份`
     // 先長出第一列
     let FirstTr = document.createElement('tr');
 
@@ -18,6 +18,7 @@ function init() {
         if (i >= firstDay) {
             td.textContent = dayCount
             dayCount++
+            td.classList.add('tdHover')
         }
         FirstTr.appendChild(td)
     }
@@ -31,6 +32,7 @@ function init() {
             let td = document.createElement('td')
             if (dayCount <= monthDay) {
                 td.innerText = dayCount
+                td.classList.add('tdHover')
                 dayCount++
             }
             tr.appendChild(td)
@@ -46,12 +48,22 @@ document.querySelector('.next').addEventListener('click',nextMonth)
 
 function preMonth(){
     currentMonth = currentMonth - 1;
+    if(currentMonth < 0){
+        currentMonth = 11
+        currentYear = currentYear - 1
+    }
     firstDay = new Date(currentYear, currentMonth, 1).getDay()
     monthDay = new Date(currentYear, currentMonth + 1, 0).getDate()
     init()
-    console.log(currentMonth, firstDay, monthDay)
 }
 
 function nextMonth() {
-    console.log(456)
+    currentMonth = currentMonth + 1;
+    if (currentMonth >= 12) {
+        currentMonth = 0
+        currentYear = currentYear + 1
+    }
+    firstDay = new Date(currentYear, currentMonth, 1).getDay()
+    monthDay = new Date(currentYear, currentMonth + 1, 0).getDate()
+    init()
 }
